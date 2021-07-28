1f42be03edce74cc086c349322a43d9b9225ac36
Boyang Han <2528314H@student.gla.ac.uk>
2021-07-28 20:52:27 +0800

    chapter7

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,10 @@
+from rango.forms import CategoryForm
 from typing import ContextManager
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+
 
 
 
@@ -40,3 +42,16 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return  render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
